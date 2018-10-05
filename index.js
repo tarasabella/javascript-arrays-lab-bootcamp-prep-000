6fd378417172010778ae806f5e0f1595164bdc0e
Tara E. Sabella <tarasabella@gmail.com>
2018-10-05 03:39:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name) {
   return kittens.push("Ralph")
 }
+function destructivelyPrependKitten(name) {
+  return kittens.unshift("Ralph")
+}
